cb4399160458286ef9d9377403652fae597c77f0
amarchino <marchino.alessandro@gmail.com>
2022-03-19 13:26:36 +0000

    Lesson 10

---
+++ 10/gogut.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	resp, _ := http.Get("https://www.washingtonpost.com/news-technology-sitemap.xml");
+	bytes, _ := ioutil.ReadAll(resp.Body);
+
+	string_body := string(bytes);
+	resp.Body.Close();
+
+	fmt.Println(string_body);
+}
